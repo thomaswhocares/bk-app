808c83d4aecdfd61808b9f080079f90ee04f829c
thomaswhocares <thomas.steffens@outlook.com>
2019-02-25 19:52:13 +0100

    style_logic done, homepage presentable, added new routes

--- lib/main.dart
+++ lib/main.dart
@@ -6,24 +6,57 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bkapp/logic/theme/theme_bloc.dart';
 import 'package:bkapp/logic/theme/theme_state.dart';
-import 'package:bkapp/views/home_page/homepage.dart';
+import 'package:bkapp/views/home_page/home_page.dart';
 
-void main() => runApp(RootWidget());
+void main() => runApp(TopBlocProvider(MyApp()));
 
-class RootWidget extends StatefulWidget {
-  RootWidget({Key key}) : super(key: key);
-  @override
-  State<StatefulWidget> createState() => RootWidgetState();
-}
-
-class RootWidgetState extends State<RootWidget> {
-  final _themeBloc = ThemeBloc();
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-        bloc: _themeBloc,
-        child: Test());
+    return BlocBuilder(
+      bloc: BlocProvider.of<ThemeBloc>(context),
+      builder: (context, ThemeState state) {
+        return MaterialApp(
+            title: 'Flutter Demo',
+            theme: state.themeData,
+            home: Homepage(),
+            routes: <String, WidgetBuilder>{
+              "/shoolPathFinder/main": (BuildContext context) =>
+                  new ErrorPage(),
+              "/settings_page": (BuildContext context) => new ErrorPage(),
+              "/profile" : (BuildContext context) => new ErrorPage(),
+              "/404": (BuildContext context) => new ErrorPage()
+            });
+      },
+    );
+  }
+}
+
+class ErrorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("NICHT DA");
+  }
+}
+
+//---------------------Adds a top level widget that allows themeState----------------------------
+class TopBlocProvider extends StatefulWidget {
+  final Widget child;
+
+  TopBlocProvider(this.child);
+  @override
+  State<StatefulWidget> createState() => TopBlocProviderState(child);
+}
+
+class TopBlocProviderState extends State<TopBlocProvider> {
+  Widget child;
+  TopBlocProviderState(this.child);
+  final _themeBloc = ThemeBloc();
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(bloc: _themeBloc, child: child);
   }
 
   @override
@@ -32,22 +65,4 @@ class RootWidgetState extends State<RootWidget> {
     super.dispose();
   }
 }
-
-class Test extends StatelessWidget{
-  const Test ({Key key}): super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return BlocBuilder(
-            bloc: BlocProvider.of<ThemeBloc>(context),
-            builder: (context, ThemeState state) {
-              return MaterialApp(
-                title: 'Flutter Demo',
-                theme: state.themeData,
-                home: Homepage(),
-              );
-            });
-  }
-
-}
+//-------------------------------------------------------------------
---
+++ lib/views/settings_page/settings_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget{
+  SettingsPage({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => SettingsPageState();
+}
+
+class SettingsPageState extends State<SettingsPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
