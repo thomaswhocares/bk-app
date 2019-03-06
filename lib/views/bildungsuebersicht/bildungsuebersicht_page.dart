bed5bb134ea70ae9ab2978ed51cfbd15fb739a4d
thomaswhocares <thomas.steffens@outlook.com>
2019-03-06 22:07:32 +0100

    anfang der bildungsübersicht

--- lib/views/bildungsuebersicht/bildungsuebersicht_page.dart
+++ lib/views/bildungsuebersicht/bildungsuebersicht_page.dart
@@ -1,7 +1,8 @@
-import 'package:bkapp/views/common_widgets/common_widgets.dart';
+import 'package:bkapp/views/common_widgets/common_widgets.dart' as CommonWidgets;
 import 'package:flutter/material.dart';
 
 class Bildungsuebersicht extends StatefulWidget {
+  Bildungsuebersicht({Key key}): super(key : key);
   @override
   _BildungsuebersichtState createState() => _BildungsuebersichtState();
 }
@@ -10,21 +11,36 @@ class _BildungsuebersichtState extends State<Bildungsuebersicht> {
   @override
   Widget build(BuildContext context) {
 
-    List<Widget> elements = new List(8);
-
-    for (int i=0;i<8;i++){
-      elements[i]=(_Entry());
-    }
-
-    return CommonPageCollumStyle(
-      viewType: ViewType.bildungsuebersicht,
-      children: elements);
+   
+    return CommonWidgets.CommonPageCollumStyle(
+      viewType: CommonWidgets.ViewType.bildungsuebersicht,
+      children: <Widget>[
+        Expanded(child: CustomScrollView(
+          scrollDirection: Axis.vertical,
+          reverse: true,
+          slivers: <Widget>[
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context,index) => _DummyElement(),
+                childCount: 200
+              ),
+            )
+          ],
+        ),)
+      ] 
+      );
   }
 }
 
-class _Entry extends StatelessWidget {
+class _DummyElement extends StatelessWidget {
+  
+  BuildContext _context;
+  _DummyElement({Key key}): super(key: key);
+  
   @override
   Widget build(BuildContext context) {
-    return Text("Ich existiere");
+    return Container(
+      child: Text("data"),
+    );
   }
 }
